ed55d10feb4a6147686f757592a4ad3b98e46240
mar <mar.nospam@anomy.net>
2019-05-17 08:54:52 +0000

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# @hugsmidjan/gulp-images
+
+```
+npm install --save-dev @hugsmidjan/gulp-images
+```
+
+## Usage
+
+```js
+const [imagesCompress, imagesWatch] = require('@hugsmidjan/gulp-images')(opts);
+```
+
+## API / Advanced usage
+
+```js
+const imagesTaskFactory = require('@hugsmidjan/gulp-images');
+
+const options = {
+  // These are the defaults:
+  name: 'images', // the display name of the generated tasks
+  src: 'src/',
+  dist: 'pub/',
+  glob: ['i/**/*', '!i/_raw/**'], // which files to glob up as entry points
+  // svg_keepIds: false, // Treat all SVG `id=`s as significant content
+};
+
+// Create the gulp tasks based on the above options.
+const imagesTasks = imagesTaskFactory(options);
+
+// imagesTasks is a two item array...
+const [imagesCompress, imagesWatch] = imagesTasks;
+// ...but it also exposes the tasks as named properties.
+const { compress, watch } = imagesTasks;
+```
+
+
+## Magic file-name compression hints
+
+PNG and JPEG images can be forced through a lossy compression via a `---q{N}` file-name suffix. The suffix is stripped from the filename before saving in the `dist` folder.
+
+In SVG files all ID attributes are stripped away unless a `svg_keepIds: true` option is passed, or if an individual SVG file has a `---ids` file-name suffix - which then gets stripped away before saving.
+
+**Examples:**
+
+  * `src/i/photo---q60.jpg` (100% quality original)  --->  `dist/i/photo.png` (recompressed to approx. 60% quality)
+  * `src/i/image---q50.png` (24bit file)  --->  `dist/i/image.png` (png8 with at least 50% quality)
+  * `src/i/image---q50-70.png` (24bit file)  --->  `dist/i/image.png` (png8 with between 50% and 70% quality)
+  * `src/i/image---q50--d0.png` (24bit file)  --->  `dist/i/image.png` (png8 with at least 50% quality - **no dithering**)
+  * `src/i/image---ids.svg` (keep ID attributes)  --->  `dist/i/image.svg` (with ID attributes intact)
---
+++ gulp-images.js
@@ -0,0 +1,96 @@
+const { src, dest, watch } = require('gulp');
+const { notifyPipeError, normalizeOpts, prefixGlobs } = require('@hugsmidjan/gulp-utils');
+
+const defaultOpts = {
+  name: 'images', // the display name of the generated tasks
+  src: 'src/',
+  dist: 'pub/',
+  glob: ['i/**/*', '!i/_raw/**'], // which files to glob up as entry points
+  // svg_keepIds: false, // Treat all SVG `id=`s as significant content
+};
+
+const _plugins = {
+  imagemin: require('gulp-imagemin'),
+  pngquant: require('imagemin-pngquant'),
+  mozjpeg: require('imagemin-mozjpeg'),
+  rename: require('gulp-rename'),
+  foreach: require('gulp-foreach'),
+};
+
+module.exports = (opts) => {
+  opts = normalizeOpts(opts, defaultOpts);
+
+  const compressTask = () => {
+    return src(prefixGlobs(opts.glob, opts.src), { base: opts.src })
+      .pipe(notifyPipeError())
+      .pipe(
+        _plugins.foreach((stream, file) => {
+          var fileParams = file.path.match(
+            /(---q(\d{1,3}(?:-\d{1,3})?)(?:--d(0))?)\.(png|jpe?g)$/i
+          );
+          if (fileParams) {
+            if (fileParams[4].toLowerCase() === 'png') {
+              stream = stream.pipe(
+                _plugins.pngquant({
+                  speed: 1, // default: `3`
+                  quality: parseInt(fileParams[2], 10), // default `undefined` (i.e. 256 colors)
+                  floyd: parseInt(fileParams[3], 10) / 100,
+                  nofs: fileParams[3] === '0',
+                })()
+              );
+            } else {
+              stream = stream.pipe(
+                _plugins.mozjpeg({
+                  quality: fileParams[2],
+                })()
+              );
+            }
+            return stream.pipe(
+              _plugins.rename((path) => {
+                path.basename = path.basename.slice(0, -fileParams[1].length);
+              })
+            );
+          } else {
+            const hasKeepIdsSuffix = /---ids.svg$/i.test(file.path);
+            stream = stream.pipe(
+              _plugins.imagemin({
+                optimizationLevel: 4, // png
+                progressive: true, // jpg
+                interlaced: true, // gif
+                multipass: true, // svg
+                svgoPlugins:
+                  opts.svg_keepIds || hasKeepIdsSuffix
+                    ? [{ cleanupIDs: false }]
+                    : undefined,
+              })
+            );
+            if (hasKeepIdsSuffix) {
+              stream = stream.pipe(
+                _plugins.rename((path) => {
+                  path.basename = path.basename.replace(/---ids$/, '');
+                })
+              );
+            }
+            return stream;
+          }
+        })
+      )
+      .pipe(dest(opts.dist));
+  };
+  compressTask.displayName = opts.name;
+
+  const watchTask = () => {
+    let watchGlobs = opts.glob;
+    if (opts.watchGlob) {
+      watchGlobs = watchGlobs.concat(opts.watchGlob);
+    }
+    watch(prefixGlobs(watchGlobs, opts.src), { base: opts.src }, compressTask);
+  };
+  watchTask.displayName = opts.name + '_watch';
+
+  const ret = [compressTask, watchTask];
+  ret.compress = compressTask;
+  ret.watch = watchTask;
+
+  return ret;
+};
